33b27c3b147852ffc08a6976ad95c0ac3c139e3f
Simon <simonxd@gmail.com>
2015-08-21 02:36:54 -0300

    [JRD-95] ajustar problemas de identação e setup para desenvolvimento js

---
+++ src/elements/components-tree/components-tree.js
@@ -0,0 +1,35 @@
+// jshint unused:false
+'use strict';
+var CarboComponentsTree = Polymer({
+    is: 'carbo-components-tree',
+});
+
+// listeners: {
+//     'banner.tap': 'handleBannerTap'
+// },
+
+// handleBannerTap: function handleBannerTap(e) {
+    
+//     e.preventDefault();
+//     e.stopPropagation();
+    
+//     if (this.isOpen) {
+//         this.$.component.className += ' closed';
+//         this.isOpen = false;
+//     } else {
+//         this.$.component.className = this.$.component.className.replace('closed', '');
+//         this.isOpen = true;
+//     }
+// },
+
+// properties: {
+//     component: {
+//         type: Object,
+//         notify: true,
+//     },
+//     isOpen: {
+//         type: Boolean,
+//         default: true,
+//         notify: true,
+//     }
+// },
