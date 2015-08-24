20762389c0b80370b5e1f972cae2edbb4cee5103
patyjenny <lala@lala.com>
2015-08-24 20:27:21 -0300

    updates on 'header' component

--- src/index.js
+++ src/index.js
@@ -61,66 +61,66 @@
 
     carbo.boxmenuitem = menu;
 
-    window.addEventListener('WebComponentsReady', function () {
-        
-        var headerLeftSection = document.getElementById('header-left-section');
-        var headerPanel = document.getElementById('carbo-header-panel');
-        var closeHeaderPanel = document.getElementById('close-header-panel');
-        var windowHeight = window.innerHeight;
-
-        var headerPanelStatus = 'inactive';
-
-        window.addEventListener('resize', function() {
-        console.log('resized')
-        windowHeight = window.innerHeight;
-        closeHeaderPanel.style.height = windowHeight;
-        });
-
-
-
-
-
-
-        headerLeftSection.addEventListener('click', function () {
-
-            if (headerPanelStatus === 'inactive') {
-                headerPanelStatus = 'active';
-                headerPanel.className = 'has-shadow active';
-                closeHeaderPanel.className = 'active';
-                closeHeaderPanel.style.height = windowHeight;
-            } else {
-                headerPanelStatus = 'inactive';
-                headerPanel.className = '';
-                closeHeaderPanel.className = '';
-            }
-
-        });
-
-        closeHeaderPanel.addEventListener('click', function () {
-        headerPanelStatus = 'inactive';
-        headerPanel.className = '';
-        closeHeaderPanel.className = '';
-
-        });
-
-
-//        var addScreenButton = document.getElementById('botao-lu');
-        var addScreenButton = document.getElementsByClassName('add-screen-button');
-        var carboSection = document.getElementsByClassName('carbo-section');
-
-        for (var i=0; i < addScreenButton.length; i++) {
-            addScreenButton[i].addEventListener('click', function (event) {
-            alert("foi");
-                console.log(event);
-
-//                event.srcElement.parent.
-//          carboSection.innerHTML = '<div class="carbo-section-contents">';
-
-          });
-        }
-
-
-    });
+//    window.addEventListener('WebComponentsReady', function () {
+//
+//        var headerLeftSection = document.getElementById('header-left-section');
+//        var headerPanel = document.getElementById('carbo-header-panel');
+//        var closeHeaderPanel = document.getElementById('close-header-panel');
+//        var windowHeight = window.innerHeight;
+//
+//        var headerPanelStatus = 'inactive';
+//
+//        window.addEventListener('resize', function() {
+//        console.log('resized')
+//        windowHeight = window.innerHeight;
+//        closeHeaderPanel.style.height = windowHeight;
+//        });
+//
+//
+//
+//
+//
+//
+//        headerLeftSection.addEventListener('click', function () {
+//
+//            if (headerPanelStatus === 'inactive') {
+//                headerPanelStatus = 'active';
+//                headerPanel.className = 'has-shadow active';
+//                closeHeaderPanel.className = 'active';
+//                closeHeaderPanel.style.height = windowHeight;
+//            } else {
+//                headerPanelStatus = 'inactive';
+//                headerPanel.className = '';
+//                closeHeaderPanel.className = '';
+//            }
+//
+//        });
+//
+//        closeHeaderPanel.addEventListener('click', function () {
+//        headerPanelStatus = 'inactive';
+//        headerPanel.className = '';
+//        closeHeaderPanel.className = '';
+//
+//        });
+//
+//
+////        var addScreenButton = document.getElementById('botao-lu');
+//        var addScreenButton = document.getElementsByClassName('add-screen-button');
+//        var carboSection = document.getElementsByClassName('carbo-section');
+//
+//        for (var i=0; i < addScreenButton.length; i++) {
+//            addScreenButton[i].addEventListener('click', function (event) {
+//            alert("foi");
+//                console.log(event);
+//
+////                event.srcElement.parent.
+////          carboSection.innerHTML = '<div class="carbo-section-contents">';
+//
+//          });
+//        }
+//
+//
+//    });
 
 
 
