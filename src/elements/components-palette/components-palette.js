7348172896f42621b85966f98469532547c8d4c7
Luciana Heuko <lalalala@lalala.com>
2015-08-24 17:56:37 -0300

    create carbo-components-palette

---
+++ src/elements/components-palette/components-palette.js
@@ -0,0 +1,82 @@
+// jshint unused:false
+'use strict';
+var CarboComponentsPalette = Polymer({
+    is: 'carbo-components-palette',
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: setup event listeners
+
+// listeners: {
+//     'banner.tap': 'handleBannerTap'
+// },
+
+// handleBannerTap: function handleBannerTap(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     if (this.isOpen) {
+//         this.$.component.className += ' closed';
+//         this.isOpen = false;
+//     } else {
+//         this.$.component.className = this.$.component.className.replace('closed', '');
+//         this.isOpen = true;
+//     }
+// },
+
+// properties: {
+//     component: {
+//         type: Object,
+//         notify: true,
+//     },
+//     isOpen: {
+//         type: Boolean,
+//         default: true,
+//         notify: true,
+//     }
+// },
