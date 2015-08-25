a7fc328df5daa212517842fcb170e2553ac77947
Luciana Heuko <lalalala@lalala.com>
2015-08-25 09:52:58 -0300

    position of preview on components palette

--- src/elements/components-palette/components-palette.js
+++ src/elements/components-palette/components-palette.js
@@ -2,81 +2,45 @@
 'use strict';
 var CarboComponentsPalette = Polymer({
     is: 'carbo-components-palette',
+    handleComponentMouseOver: function(event) {
 
+        event = Polymer.dom(event)
+
+        var componentTarget = event.rootTarget;
+
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+
+        var previewWidth = this.$.preview.offsetWidth;
+        var previewHeight = this.$.preview.offsetHeight;
+
+        var componentTargetWidth = componentTarget.offsetWidth;
+        var componentTargetHeight = componentTarget.offsetHeight;
+        var componentTargetLeftPosition = componentTarget.offsetLeft;
+        var componentTargetTopPosition = componentTarget.offsetTop;
+
+//        var distanceToBorderRight = componentTargetRightPosition - componentTargetWidth;
+        var distanceToBottom = windowHeight - componentTargetTopPosition;
+
+        if (previewWidth <= componentTargetLeftPosition) {
+            console.log("espaço menor");
+            this.$.preview.style.right = componentTargetLeftPosition + "px";
+            console.log(componentTargetLeftPosition);
+        } else {
+            console.log("espaço maior");
+            this.$.preview.style.right = componentTargetLeftPosition - previewWidth + "px";
+            console.log(componentTargetLeftPosition);
+        }
+
+//        if (previewHeight <= distanceToBottom) {
+//            this.$.preview.style.top = windowHeight - previewHeight + "px";
+//            console.log("altura menor");
+//            console.log("y:" + componentTargetTopPosition);
+//            console.log(previewHeight);
+//        } else {
+//            this.$.preview.style.top = componentTargetTopPosition + "px";
+//            console.log("altura maior");
+//            console.log(previewHeight);
+//        }
+    }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TODO: setup event listeners
-
-// listeners: {
-//     'banner.tap': 'handleBannerTap'
-// },
-
-// handleBannerTap: function handleBannerTap(e) {
-//     e.preventDefault();
-//     e.stopPropagation();
-//     if (this.isOpen) {
-//         this.$.component.className += ' closed';
-//         this.isOpen = false;
-//     } else {
-//         this.$.component.className = this.$.component.className.replace('closed', '');
-//         this.isOpen = true;
-//     }
-// },
-
-// properties: {
-//     component: {
-//         type: Object,
-//         notify: true,
-//     },
-//     isOpen: {
-//         type: Boolean,
-//         default: true,
-//         notify: true,
-//     }
-// },
