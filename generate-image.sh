1743b43fab268faba957bfd8c0559c355109f85a
Renan Dias <renan.dias@fabricadeaplicativos.com.br>
2015-08-25 09:41:31 -0300

    [JRD-170] Setup script to build and push image to our private registry

--- generate-image.sh
+++ generate-image.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+if [[ $DOCKER_REGISTRY != "10.0.1.120:500" ]]; then
+	echo "Esse script necessita da variável de ambiente DOCKER_REGISTRY.";
+	echo "Utilize o comando abaixo para resolver o problema:";
+	echo "    * export DOCKER_REGISTRY=10.0.1.120:5000";
+	echo;
+	echo "Certifique-se também de que esta máquina esta conectada em nossa VPN."
+
+	exit 1;
+fi
+
+set -x
 
 npm install
 bower install
-docker build -t local/ide .
+docker build -t $DOCKER_REGISTRY/ide .
+docker push $DOCKER_REGISTRY/ide
