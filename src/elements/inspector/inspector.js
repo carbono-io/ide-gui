83ff3272af26a55f0ea1b6e530b09f584cac275f
Simon <simonxd@gmail.com>
2015-08-27 23:22:00 -0300

    [JRD-95] application interaction inspector working

--- src/elements/inspector/inspector.js
+++ src/elements/inspector/inspector.js
@@ -3,20 +3,33 @@
     Polymer({
         is: 'carbo-inspector',
         ready: function () {
+            
+            window.addEventListener('message', this.handleFrameMessage.bind(this), false);
 
+//            document.addEventListener('mousemove', this.handleMousemove.bind(this));
 
-            document.addEventListener('mousemove', this.handleMousemove.bind(this));
+            document.addEventListener('mouseout', function (e) {
+                var from = e.relatedTarget || e.toElement;
+                if (!from || from.nodeName == "HTML") {
+                    this.unHighlight();
+                }
+            }.bind(this));
 
-
-
-
-//            document.addEventListener('mouseout', function (e) {
-//                var from = e.relatedTarget || e.toElement;
-//                if (!from || from.nodeName == "HTML") {
-//                    this.unHighlight();
-//                }
-//            }.bind(this));
-
+        },
+        
+        handleFrameMessage: function (event) {
+            var data = JSON.parse(event.data);
+            
+            var operationName = data.operation;
+            var args          = data.args || [];
+            
+            var operation = this[operationName];
+            
+            if (operation) {
+                operation.apply(this, args);
+            } else {
+                console.warn('Operation %s not defined at inspector', operationName);
+            }
         },
 
         handleMousemove: function (event) {
@@ -27,60 +40,27 @@
                 y: event.clientY
             };
 
-            // get hovered component (Element under that position)
-            var element = document.elementFromPoint(pos.x, pos.y);
-
-            console.log(element.id);
-
-//            if (element === this) {
-//                return;
-//            }
-
-            // salva a div highlighter numa variável
-            var highlighter = this.$.highlighter;
-            var clickarea = this.$.clickarea;
-//            console.log(highlighter);
-
-            //
-            if (this.activeElement) {
-                this.unHighlight(this.activeElement);
-            }
-
             // faz a função highlight no elemento que ele guardou na variavel 'element'
-            this.highlight(element, highlighter, clickarea);
+            this.highlightElementAtPoint(pos);
 
-            // 'ativa' o elemento que ele guardou na variavel 'element': Returns the currently focused element, that is, the element that will get keystroke events if the user types any. This attribute is read only.
-            // Set active element
-            this.activeElement = element;
         },
 
         // *****end handlemousemove
 
-        highlight: function (element, highlighter, clickarea) {
-            // if (this.activeElement === element) {
-            //     return;
-            // }
+        highlight: function (element) {
+            
+            if (this.activeElement) {
+                this.unHighlight(this.activeElement);
+            }
 
-//            console.log('highlight');
-//            console.log(element)
-
-//            element.style.border = '4px red dashed';
-
-
-            this.toggleClass('show', true, this.$.highlighter);
-            this.toggleClass('show', true, this.$.clickarea);
-
-
-//            // get element's size dynamically
-//            var elementWidth = element.offsetWidth;
-//            var elementHeight = element.offsetHeight;
-//
-//
-//            // get left-position of element
-//            var elementLeftPosition = element.offsetLeft;
-//            // get height-position of element
-//            var elementTopPosition = element.offsetTop;
-
+            // 'ativa' o elemento que ele guardou na variavel 'element': Returns the currently focused element, that is, the element that will get keystroke events if the user types any. This attribute is read only.
+            // Set active element
+            this.activeElement = element;
+            
+            var highlighter = this.$.highlighter;
+            
+            this.toggleClass('show', true, highlighter);
+//            this.toggleClass('show', true, this.$.clickarea);
 
             var rect = element.getBoundingClientRect();
 
@@ -89,35 +69,33 @@
 
             highlighter.style.width = rect.width + "px";
             highlighter.style.height = rect.height + "px";
-
-            clickarea.style.left = rect.left + "px";
-            clickarea.style.top = rect.top + "px";
-
-            clickarea.style.width = rect.width + "px";
-            clickarea.style.height = rect.height + "px";
-
-
-//            console.log(rect.left);
-
-//            element.style.pointerEvents = none;
-
         },
         // *****end highlight
 
-        unHighlight: function (element) {
-
-//             if (this.activeElement === element) {
-//                 return;
-//             }
-
+        unHighlight: function () {
             this.toggleClass('show', false, this.$.highlighter);
-            this.toggleClass('show', false, this.$.clickarea);
-
-//              element.style.pointerEvents = all;
-
         },
         //*****end unhighlight
-
+        
+        highlightElementAtPoint: function (point) {
+            // get hovered component (Element under that position)
+            var element = document.elementFromPoint(point.x, point.y);
+            
+            if (element === this) {
+                this.unHighlight();
+            }
+            
+            this.highlight(element);
+        },
+        
+        
+        scrollBy: function (deltaX, deltaY) {
+            
+//            console.log('scroll x: %s, y: %s', deltaX, deltaY);
+            window.scrollBy(deltaX, deltaY);
+            
+            this.unHighlight();
+        }
     })
 
 })();
