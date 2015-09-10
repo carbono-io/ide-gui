bc7055539e526a6d04588375af7619a4032dab1f
patyjenny <lala@lala.com>
2015-09-10 18:10:06 -0300

    data-table

--- src/elements/data-table/data-table.js
+++ src/elements/data-table/data-table.js
@@ -4,6 +4,43 @@
     Polymer({
         is: 'carbo-data-table',
 
+        ready: function() {
+                    var entityId = this.$$('#entity-id');
+                    console.log(entityId);
+                    var componentContext = this;
+
+                    var isClosed = !this.closed;
+
+                    if (isClosed) {
+                        console.log('open');
+                        Polymer.Base.toggleClass('closed', false, componentContext.$$('#entity'));
+                        isClosed = false;
+                    } else {
+                        console.log('close');
+                        Polymer.Base.toggleClass('closed', true, componentContext.$$('#entity'));
+                        isClosed = true;
+                    }
+
+                    entityId.addEventListener('click', function(event) {
+
+                        if (isClosed) {
+                            console.log('open');
+                            Polymer.Base.toggleClass('closed', false, componentContext.$$('#entity'));
+                            isClosed = false;
+                        } else {
+                            console.log('close');
+                            Polymer.Base.toggleClass('closed', true, componentContext.$$('#entity'));
+                            isClosed = true;
+                        }
+                    });
+                },
+
+
+
+
+
+
+
     });
 
 })();
