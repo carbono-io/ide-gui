82f8e36d5e8517b23ad74ee7beb4833013dbd2d1
Luciana Heuko <lalalala@lalala.com>
2015-09-14 09:02:54 -0300

    toggle class style box

--- src/elements/styles-list/styles-list.js
+++ src/elements/styles-list/styles-list.js
@@ -6,44 +6,65 @@
 
         ready: function() {
 
-            console.log('!!!!!!');
-            console.log(this.group);
+//
+//            console.log('!!!!!!');
+//            console.log(this.group);
+//
+//            var groupName = this.$$('#group-name');
+//            var componentContext = this;
+//
+//            var isClosed = !this.closed;
+//
+//            if (isClosed) {
+//                console.log('isclose')
+//
+//                console.log(componentContext);
+//
+//                var toBeClosed = componentContext.$$('#group');
+//
+//                console.log(toBeClosed)
+//
+//                Polymer.Base.toggleClass('closed', false, toBeClosed);
+//
+//                console.log('qweqweqweqw');
+//                isClosed = false;
+//            } else {
+//                console.log('isnotcloses')
+//                Polymer.Base.toggleClass('closed', true, componentContext.$$('#group'));
+//                isClosed = true;
+//            }
+//
+//            groupName.addEventListener('click', function(event) {
+//
+//                if (isClosed) {
+//                    Polymer.Base.toggleClass('closed', false, componentContext.$$('#group'));
+//                    isClosed = false;
+//                } else {
+//                    Polymer.Base.toggleClass('closed', true, componentContext.$$('#group'));
+//                    isClosed = true;
+//                }
+//            });
+        },
+        toggleClosedClass: function (event) {
 
-            var groupName = this.$$('#group-name');
-            var componentContext = this;
+            var groupNode = event.currentTarget.parentNode;
+            var isClosed = groupNode.matches(".closed");
 
-            var isClosed = !this.closed;
-
-            if (isClosed) {
-                console.log('isclose')
-
-                console.log(componentContext);
-
-                var toBeClosed = componentContext.$$('#group');
-
-                console.log(toBeClosed)
-
-                Polymer.Base.toggleClass('closed', false, toBeClosed);
-
-                console.log('qweqweqweqw');
+            if(isClosed == true) {
+                Polymer.Base.toggleClass('closed', false, groupNode);
                 isClosed = false;
+
             } else {
-                console.log('isnotcloses')
-                Polymer.Base.toggleClass('closed', true, componentContext.$$('#group'));
+                Polymer.Base.toggleClass('closed', true, groupNode);
                 isClosed = true;
             }
-
-            groupName.addEventListener('click', function(event) {
-
-                if (isClosed) {
-                    Polymer.Base.toggleClass('closed', false, componentContext.$$('#group'));
-                    isClosed = false;
-                } else {
-                    Polymer.Base.toggleClass('closed', true, componentContext.$$('#group'));
-                    isClosed = true;
-                }
-            });
         },
+
+        setClassName: function () {
+            var iconName = this.$$('.style-preview-2');
+            console.log(iconName);
+        },
+
     });
 
 })();
