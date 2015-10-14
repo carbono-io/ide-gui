a1840ee625a25eee4342691d72fa687f1a2d44dd
Simon <simonxd@gmail.com>
2015-10-14 11:55:01 -0300

    updated spotlighter version

--- src/elements/canvas/scripts/behaviors/spotlighter.js
+++ src/elements/canvas/scripts/behaviors/spotlighter.js
@@ -1,33 +1,43 @@
 'use strict';
 
-// HACK
-// TODO: let spotlighter be hidden at start
-exports.ready = function () {
-
-    this.$.spotlighter.hide();
-};
-
+/**
+ * Sets the spotlight upon an element
+ * @param  {CSSSelector} selector
+ */
 exports.spotlight = function (selector) {
 
     var spotlighter = this.$.spotlighter;
 
-    this.getElementsData(selector)
+    return this.getElementsData(selector)
         .then(function (elementsData) {
             var rects = elementsData.map(function (data) {
-                return this.normalizeBoundingRect(data.rect);
+                return this._normalizeBoundingRect(data.rect);
             }.bind(this));
-
-            console.log(rects);
 
             spotlighter.setTargetBoundingRects(rects);
             spotlighter.show();
 
-        }.bind(this))
-        .done();
-
+        }.bind(this));
 };
 
-exports.normalizeBoundingRect = function (rect) {
+/**
+ * Hides the spotlighter
+ */
+exports.hideSpotlight = function () {
+    this.$.spotlighter.hide();
+};
+
+/**
+ * Shows the spotlighter
+ */
+exports.showSpotlight = function () {
+    this.$.spotlighter.show();
+};
+
+/**
+ * Calculates the bounding rect from a given rect within the iframe
+ */
+exports._normalizeBoundingRect = function (rect) {
     // Calculate the rect of the iframe
     var iframeRect = this.$.iframe.getBoundingClientRect();
 
