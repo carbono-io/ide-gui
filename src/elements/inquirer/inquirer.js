9d5a03e7d1e1f79ef92410c971cd36cff5838fe4
Simon <simonxd@gmail.com>
2015-10-14 19:34:22 -0300

    development environment for carbo-inquirer

--- src/elements/inquirer/inquirer.js
+++ src/elements/inquirer/inquirer.js
@@ -5,35 +5,10 @@
  * asking questions to the user.
  */
 
-/**
-FROM: inquirer (node module)
-
-type: (String) Type of the prompt. Defaults: input - Possible values: input, confirm, list, rawlist, password
-name: (String) The name to use when storing the answer in the answers hash.
-message: (String|Function) The question to print. If defined as a function, the first parameter will be the current inquirer session answers.
-default: (String|Number|Array|Function) Default value(s) to use if nothing is entered, or a function that returns the default value(s). If defined as a function, the first parameter will be the current inquirer session answers.
-choices: (Array|Function) Choices array or a function returning a choices array. If defined as a function, the first parameter will be the current inquirer session answers.
-Array values can be simple strings, or objects containing a name (to display) and a value properties (to save in the answers hash). Values can also be a Separator.
-validate: (Function) Receive the user input and should return true if the value is valid, and an error message (String) otherwise. If false is returned, a default error message is provided.
-filter: (Function) Receive the user input and return the filtered value to be used inside the program. The value returned will be added to the Answers hash.
-when: (Function, Boolean) Receive the current user answers hash and should return true or false depending on whether or not this question should be asked. The value can also be a simple boolean.
- */
+// external dependencies
+var Q = require('q');
 
 // CONSTANTS
-var MOCK_QUESTIONS = [];
-MOCK_QUESTIONS.push({
-    type: 'input',  // input, confirm, list, rawlist, password
-    message: 'Qual o seu nome?'
-});
-MOCK_QUESTIONS.push({
-    type: 'list',
-    message: 'Qual o seu chocolate preferido?'
-});
-MOCK_QUESTIONS.push({
-    type: 'password',
-    message: 'Qual o nome do seu cachorro?'
-});
-
 var CanvasIntegrationBehavior = require('./scripts/behaviors/canvas-integration');
 
 Polymer({
@@ -49,7 +24,7 @@ Polymer({
             notify: true,
             observer: '_handleQuestionsChange',
 
-            value: MOCK_QUESTIONS,
+            value: [],
         },
 
         /**
@@ -63,6 +38,10 @@ Polymer({
             value: 0,
         },
 
+        /**
+         * Whether the inquirer is at the last question
+         * @type {Object}
+         */
         isAtLastQuestion: {
             type: Boolean,
             notify: true,
@@ -70,6 +49,10 @@ Polymer({
             value: false,
         },
 
+        /**
+         * Whether the inquirer is at the first question
+         * @type {Object}
+         */
         isAtFirstQuestion: {
             type: Boolean,
             notify: true,
@@ -81,6 +64,38 @@ Polymer({
     behaviors: [
         CanvasIntegrationBehavior,
     ],
+
+    prompt: function (questions) {
+        // cleanup
+        delete this.defer;
+
+        var defer = Q.defer();
+
+        // save reference to the defer object
+        this.defer = defer;
+
+        // set questions
+        this.set('questions', questions);
+
+        // return promise
+        return defer.promise;
+    },
+
+    readAnswers: function () {
+        var questions = this.get('questions');
+        return questions.reduce(function (answers, question) {
+            answers[question.name] = question.value;
+        }, {});
+    },
+
+    submit: function () {
+
+        var answers = this.readAnswers();
+
+        console.log(answers);
+
+        console.log('submit');
+    },
 
     /**
      * Question navigation
