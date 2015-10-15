33c3e48cc3e18509d5a976b759b73fe83ccbbc99
patyjenny <lala@lala.com>
2015-10-15 13:42:48 -0300

    iconset in progress

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,17 @@ require('./tasks/setup')(gulp, $);
 require('./tasks/develop')(gulp, $);
 require('./tasks/linting')(gulp, $);
 
+var polymerizeSvg = require('./tasks/gulp-polymerize-svg');
+
+// generate-iconset
+gulp.task('iconset', function () {
+
+    gulp.src('src/assets/icons/actions/**/*')
+        .pipe(polymerizeSvg())
+        .pipe(gulp.dest('_teste'));
+//    polymerizeSvg();
+});
+
 // do not show at help list
 gulp.task('default', false, ['help'], function () {
     console.log('\nRun ' + $.util.colors.white.bgGreen('gulp develop') + ' if you are lost :)\n');
